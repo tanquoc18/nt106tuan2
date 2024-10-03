6e3dc9a2989780bcf6cd39617394df8a8dfed557
Nhatkhanhk <23520714@gm.uit.edu.vn>
2024-10-03 11:43:58 +0700

    up bt

---
+++ UDPChatClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPClient
+{
+    static void Main()
+    {
+        // Tạo socket cho client
+        Socket udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPAddress serverIP = IPAddress.Parse("127.0.0.1"); // Địa chỉ IP của server
+        IPEndPoint remoteEP = new IPEndPoint(serverIP, 11000);
+
+        while (true)
+        {
+            Console.Write("Nhập tin nhắn: ");
+            string message = Console.ReadLine();
+
+            // Gửi dữ liệu đến server
+            byte[] sendBytes = Encoding.UTF8.GetBytes(message);
+            udpClient.SendTo(sendBytes, remoteEP);
+
+            // Nhận phản hồi từ server
+            byte[] buffer = new byte[1024];
+            EndPoint serverEP = new IPEndPoint(IPAddress.Any, 0);
+            int receivedBytes = udpClient.ReceiveFrom(buffer, ref serverEP);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+            Console.WriteLine(receivedMessage);
+        }
+    }
+}
---
+++ UDPChatServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPServer
+{
+    static void Main()
+    {
+        // Tạo UdpClient để lắng nghe trên cổng 11000
+        UdpClient udpServer = new UdpClient(11000);
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 11000);
+
+        Console.WriteLine("Server đang lắng nghe...");
+
+        while (true)
+        {
+            // Nhận dữ liệu từ client
+            byte[] receiveBytes = udpServer.Receive(ref remoteEP);
+            string receivedData = Encoding.UTF8.GetString(receiveBytes);
+            Console.WriteLine($"Client: {receivedData}");
+
+            // Gửi phản hồi lại cho client
+            string responseData = $"Server đã nhận được: {receivedData}";
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseData);
+            udpServer.Send(responseBytes, responseBytes.Length, remoteEP);
+        }
+    }
+}
